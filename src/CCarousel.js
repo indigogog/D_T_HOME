951c3d3cdea9bccb1aa51f5038b5bf396c43081d
indigogog <hamer000711@gmail.com>
2021-11-11 18:03:40 +0300

    small fix

--- src/CCarousel.js
+++ src/CCarousel.js
@@ -2,43 +2,85 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "./CCarousel.css";
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 export default function CCarousel() {
   return (
-    <div>
-      <Carousel variant="dark" className="CCC">
-        <Carousel.Item>
+    <div style={{ display: 'inline-block', width: 700, padding: 30, margin: 26}}>
+      <Carousel>
+        <Carousel.Item interval={1500}>
           <img
-            className="d-block_w-100"
-            src="https://sm.ign.com/t/ign_latam/screenshot/default/toretto-espacio_twu9.1280.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <p className="quote">Ты всегда будешь со мной, ты всегда будешь моим братом.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block_w-100"
+            className="d-block w-100"
+            height="300" 
             src="https://s3.wi-fi.ru/cp3o/6kNViT78U6gJraAAbW11ZmZp?response-content-type=image/jpeg"
-            alt="Second slide"
+            alt=""
           />
           <Carousel.Caption>
-          <p className="quote">В уличных драках всегда побеждает улица.</p>
+            <h3>Братья.</h3>
+            <p>Ты всегда будешь со мной, ты всегда будешь моим братом.</p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+        <Carousel.Item interval={1500}>
           <img
-            className="d-block_w-100"
-            src="https://www.film.ru/sites/default/files/filefield_paths/dnh_fastfuriousspace_110717_1120.jpg"
-            alt="Third slide"
+            className="d-block w-100"
+            height="300"
+            src="https://sm.ign.com/t/ign_latam/screenshot/default/toretto-espacio_twu9.1280.jpg"
+            alt=""
           />
           <Carousel.Caption>
-          <p className="quote">
-              Нельзя отворачиваться от семьи, даже если она отвернулась от тебя.
-            </p>
+            <h3>Улица.</h3>
+            <p>В уличных драках всегда побеждает улица.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+            height="300"
+            src="https://www.film.ru/sites/default/files/filefield_paths/dnh_fastfuriousspace_110717_1120.jpg"
+            alt=""
+          />
+          <Carousel.Caption>
+            <h3>Семья.</h3>
+            <p>Нельзя отворачиваться от семьи, даже если она отвернулась от тебя.</p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     </div>
+    // <div>
+    //   <Carousel variant="dark" className="CCC">
+    //     <Carousel.Item>
+    //       <img
+    //         className="d-block_w-100"
+    //         src="https://sm.ign.com/t/ign_latam/screenshot/default/toretto-espacio_twu9.1280.jpg"
+    //         alt="First slide"
+    //       />
+    //       <Carousel.Caption>
+    //         <p className="quote">Ты всегда будешь со мной, ты всегда будешь моим братом.</p>
+    //       </Carousel.Caption>
+    //     </Carousel.Item>
+    //     <Carousel.Item>
+    //       <img
+    //         className="d-block_w-100"
+    //         src="https://s3.wi-fi.ru/cp3o/6kNViT78U6gJraAAbW11ZmZp?response-content-type=image/jpeg"
+    //         alt="Second slide"
+    //       />
+    //       <Carousel.Caption>
+    //       <p className="quote">В уличных драках всегда побеждает улица.</p>
+    //       </Carousel.Caption>
+    //     </Carousel.Item>
+    //     <Carousel.Item>
+    //       <img
+    //         className="d-block_w-100"
+    //         src="https://www.film.ru/sites/default/files/filefield_paths/dnh_fastfuriousspace_110717_1120.jpg"
+    //         alt="Third slide"
+    //       />
+    //       <Carousel.Caption>
+    //       <p className="quote">
+    //           Нельзя отворачиваться от семьи, даже если она отвернулась от тебя.
+    //         </p>
+    //       </Carousel.Caption>
+    //     </Carousel.Item>
+    //   </Carousel>
+    // </div>
   );
 }
--- src/CrosCardComponent.js
+++ src/CrosCardComponent.js
@@ -9,42 +9,41 @@ import Button from "react-bootstrap/Button";
 export default class CrosCardComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.props.func();
-    this.carddata = this.carddata.bind(this);
+    this.state = {
+      data: MyJson
+    }
+    this.lookItem = this.lookItem.bind(this);
   }
 
-
-  carddata() {
-    MyJson.map((MyJson) => {
-      console.log(MyJson.key);
-      return (
-        <Card
-          bg="dark"
-          key={MyJson.key}
-          text="white"
-          style={{ width: "18rem" }}
-          className="mb-2"
-          border="primary"
-        >
-          <Card.Img variant="top" src={MyJson.src} className="CardImage" />
-          <Card.Body>
-            <Card.Title>{MyJson.title} </Card.Title>
-            <Card.Text>{MyJson.text}</Card.Text>
-            <Button variant="primary" onClick={this.props.func}>
-              Посмотреть
-            </Button>
-          </Card.Body>
-        </Card>
-      );
-    });
+  lookItem(itemIndex) {
+    console.log(itemIndex)
   }
+
   render() {
     return (
       <div>
         <ul className="try">
           <CardGroup>
             <Row xs={1} md={1} className="g-4">
-              {this.carddata()}
+              {this.state.data.map((item) => (
+                <Card
+                  bg="dark"
+                  key={item.key}
+                  text="white"
+                  style={{ width: "18rem" }}
+                  className="mb-2"
+                  border="primary"
+                >
+                  <Card.Img variant="top" src={item.src} className="CardImage" />
+                  <Card.Body>
+                    <Card.Title>{item.title} </Card.Title>
+                    <Card.Text>{item.text}</Card.Text>
+                    <Button variant="primary" onClick={() => this.lookItem(item.key)}>
+                      Посмотреть
+                    </Button>
+                  </Card.Body>
+                </Card>
+              ))}
             </Row>
           </CardGroup>
           {/* <div onLoad={this.DoNothing()}></div> */}
