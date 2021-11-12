32d7ad0a8a5c1e0654adbd89145b3f4caa43264e
indigogog <hamer000711@gmail.com>
2021-11-12 16:32:33 +0300

    nerobit

--- src/CrosCardComponent.js
+++ src/CrosCardComponent.js
@@ -10,14 +10,37 @@ export default class CrosCardComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: MyJson
-    }
+      data: MyJson,
+      buttons_condition: [],
+      items: [],
+    };
     this.lookItem = this.lookItem.bind(this);
+    this.button_text_render = this.button_text_render.bind(this);
+    // this.push_in_state = this.push_in_state.bind(this);
   }
 
   lookItem(itemIndex) {
-    console.log(itemIndex)
+    console.log(itemIndex);
   }
+
+  button_text_render(key) {
+    if(this.state.data[key].condition==="false"){
+      this.state.data[key].condition="true"
+      console.log("PIZDA");
+      return ("Посмотреть");
+    }
+    else  {
+      console.log("PIZDA");
+      return ("В корзине");
+    }
+  }
+  // push_in_state() {
+  //   this.setState({
+  //     buttons_condition: [...this.state.buttons_condition, false],
+  //   });
+  //   console.log("PIZDA");
+  //   console.log(this.state.buttons_condition.lenght);
+  // }
 
   render() {
     return (
@@ -34,12 +57,19 @@ export default class CrosCardComponent extends React.Component {
                   className="mb-2"
                   border="primary"
                 >
-                  <Card.Img variant="top" src={item.src} className="CardImage" />
+                  <Card.Img
+                    variant="top"
+                    src={item.src}
+                    className="CardImage"
+                  />
                   <Card.Body>
                     <Card.Title>{item.title} </Card.Title>
                     <Card.Text>{item.text}</Card.Text>
-                    <Button variant="primary" onClick={() => this.lookItem(item.key)}>
-                      Посмотреть
+                    <Button
+                      variant="primary"
+                      onClick={() => this.props.func()}
+                    >
+                      {this.button_text_render(item.key)}
                     </Button>
                   </Card.Body>
                 </Card>
